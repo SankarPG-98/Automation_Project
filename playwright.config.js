5ca96d9e003a3fba7907a70d720a3288eacc24d7
SankarPG-98 <visnusankar98@gmail.com>
2024-10-10 14:12:37 +0530

    Initial commit

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+const config = {
+
+
+  testDir: './tests',
+  timeout : 30*1000,
+  except :{
+    timeout : 5000,
+  },
+
+  reporter : 'html',
+  
+  use: {
+   browserName:'chromium'
+
+  }
+
+  
+};
+
